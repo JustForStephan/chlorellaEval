132d4c70f19362af41a0618af2345844867bcbf7
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2023-11-19 14:27:12 +0100

    fixed training network
    Result: The training of the networks works, but the test of the algorithm shows, that the applied linear regression model is not that accurate for this problem.
    After defining the weights of the network, the input frequencies where multiplied to the weights and they have significant derivations to the original ones in the dataset. Thats why another network is needed to increase the accuracy.
    On other hand the both weights are quite similar, thats why the datasets of both frequencies with both photosynthetic acitivities get calculated together in future.

--- main.py
+++ main.py
@@ -16,23 +16,24 @@ def estetics_in_json(string):                   # makes the json readable before
     return string
 
 def training_neural_network():
-    w = [random.randint(-1, 1), random.randint(-1, 1)]
 
-    frequency1 = read_json("frequency1")
-    frequency2 = read_json("frequency2")
-    photosynthetic_activity1 = read_json("photosynthetic_activity1")
-    photosynthetic_activity2 = read_json("photosynthetic_activity2")
-    running_time = read_json("running_time_training")
-    etha = read_json("etha")
+    w = [float(random.randrange(-1, 1)), float(random.randint(-1, 1))]
 
-    for running_index in range(running_time):
-        for i in range(len(frequency1)):
-            y1 = frequency1[i]*w[0]
-            y2 = frequency2[i]*w[1]
-            error1 = photosynthetic_activity1[i]-y1
-            error2 = photosynthetic_activity2[i]-y2
-            w[0]+= 2*error1*frequency1[i]*etha
-            w[1]+= 2*error2*frequency2[i]*etha
+    print("Start training of neural network:")
+    print("----------------------------------")
+    for running_index in range(read_json("running_time_training")):
+        for i in range(len(read_json("frequency1"))):
+            y1 = float(read_json("frequency1")[i])*w[0]
+            y2 = float(read_json("frequency2")[i])*w[1]
+            error1 = float(read_json("photosynthetic_activity1")[i])-y1
+            error2 = read_json("photosynthetic_activity2")[i]-y2
+            w[0]+= 2*error1*read_json("frequency1")[i]*read_json("etha")
+            w[1]+= 2*error2*read_json("frequency2")[i]*read_json("etha")
+        print("running_index: "+str(running_index))
+        print("\nw1: "+str(w[0]))
+        print("w2: "+str(w[1]))
+    print("final neural weight configuration: "+str(w))
+    return w
 
 def calc_new_photosynthetic_activity(w, newFrequency):
     newActivity = []
@@ -42,16 +43,10 @@ def calc_new_photosynthetic_activity(w, newFrequency):
 
 w = training_neural_network()
 
-newFrequency1 = read_json("frequency1")
-newFrequency2 = read_json("frequency2")
-print(w)
-new_photoActivity1 = calc_new_photosynthetic_activity(w[0],newFrequency1)
-new_photoActivity2 = calc_new_photosynthetic_activity(w[1], newFrequency2)
-oldActivity1 = read_json("photosynthetic_activity1")
-oldActivity2 = read_json("photosynthetic_activity2")
+new_photoActivity1 = calc_new_photosynthetic_activity(w[0], read_json("frequency1"))
+new_photoActivity2 = calc_new_photosynthetic_activity(w[1], read_json("frequency2"))
 
-for i in range(len(newFrequency1)):
+for i in range(len(read_json("frequency1"))):
     print("--------------")
-    print(oldActivity1[i])
-    print(new_photoActivity1[i])
-
+    print(read_json("photosynthetic_activity1")[i])
+    print(str(read_json("frequency1")[i]*w[0]))

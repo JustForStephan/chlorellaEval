d4d3b94c6f4a94565c549bc06d2bb410d0cf0a75
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2023-11-25 11:23:05 +0100

    created weight matrix and calc of photosynthetic activity

--- sigmoid_regression/neurons.py
+++ sigmoid_regression/neurons.py
@@ -4,7 +4,7 @@ import numpy
 class neuron:
 
     position = "input_neuron"
-    weight = random.randrange(-1,1,0.1)
+    weight = random.random()
     z = 0
     y = 0
     x = []
@@ -14,7 +14,9 @@ class neuron:
         self.y = self.calc_y(self.z)
 
     def calc_z(self, x, w):
-        return numpy.dot(x,w)
+        z = numpy.dot(x,w)
+        if z.shape != (): z = z[0]
+        return z
 
     def calc_y(self, z):
         return 1/(1+numpy.exp((-1)*z))
--- sigmoid_regression/training_network.py
+++ sigmoid_regression/training_network.py
@@ -1,13 +1,13 @@
 import json
-import neurons
+from neurons import neuron
 import random
 
 w = [
     [1,1],
-    [random.randrange(-1,1,0.1), random.randrange(-1,1,0.1)],
-    [random.randrange(-1,1,0.1), random.randrange(-1,1,0.1)],
-    [random.randrange(-1,1,0.1)],
-    [random.randrange(-1,1,0.1)]
+    [random.random(), random.random()],
+    [random.random(), random.random()],
+    [random.random()],
+    [random.random()]
 ]
 
 def read_json(file, name):
@@ -15,13 +15,18 @@ def read_json(file, name):
     data = json.load(f)
     return data[name]
 
-
+neuron1 = neuron([read_json("vectors_new.json", "frequency")[0]], [w[0][0]])
+neuron2 = neuron([1], w[0][1])
+neuron3 = neuron([neuron1.y, neuron2.y], [w[1][0], w[2][0]])
+neuron4 = neuron([neuron1.y, neuron2.y], [w[1][1], w[2][1]])
+neuron5 = neuron([neuron3.y, neuron4.y], [w[3], w[4]])
+print(neuron5.y)
 # loop for training index
 for running_index in range(read_json("vectors_new.json", "running_time_training")):
 
     # loop for length of input
     for i in range(len(read_json("vectors_new.json", "frequency"))):
-        neuron1 = neurons([read_json("vectors_new.json", "frequency")[i]],[w[0][0]])
-        neuron2 = neurons([1],w[0][1])
-        neuron3 = neurons([neuron1.y, neuron2.y], [w[1][0], w[2][0]])
-        neuron4 = neurons([neuron1.y, neuron2.y], [w[1][1], w[2][1]])
+        neuron1 = neuron([read_json("vectors_new.json", "frequency")[i]],[w[0][0]])
+        neuron2 = neuron([1],w[0][1])
+        neuron3 = neuron([neuron1.y, neuron2.y], [w[1][0], w[2][0]])
+        neuron4 = neuron([neuron1.y, neuron2.y], [w[1][1], w[2][1]])

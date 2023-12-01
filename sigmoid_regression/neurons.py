6389d7d0b66ef77e498b9347a0603c19eaf9c867
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2023-12-01 17:59:17 +0100

    search for uptimation (without success)

--- sigmoid_regression/neurons.py
+++ sigmoid_regression/neurons.py
@@ -6,34 +6,18 @@ class neuron:
     y = 0
 
     def __init__(self, x, w):
-        self.z = self.calc_z(x,w)
-        self.y = self.calc_y(self.z)
+        self.z = None
+        self.y = None
 
     def calc_z(self, x, w):
-        z = 0
+        self.z = 0
         for i in range(len(x)):
-            z += float(x[i])*float(w[i])
-        return z
+            self.z += float(x[i])*float(w[i])
 
-    def calc_y(self, z):
-        return (1/(1+math.exp((-1)*z)))
+    def calc_y(self):
+        self.y=1.0/(1.0+math.exp(-(self.z-0)))
+        return self.y
 
     def update(self, x, w):
-        self.z = self.calc_z(x, w)
-        self.y = self.calc_y(self.z)
-
-class neuronOutput():
-
-    y = 0
-
-    def __init__(self, x, w):
-        self.y = self.calc_y(x,w)
-
-    def calc_y(self, x, w):
-        y = 0
-        for i in range(len(x)):
-            y += float(x[i])*float(w[i])
-        return y
-
-    def update(self, x, w):
-        self.y = self.calc_y(x, w)
+        self.calc_z(x, w)
+        self.calc_y()
--- sigmoid_regression/training_network.py
+++ sigmoid_regression/training_network.py
@@ -1,16 +1,9 @@
 import json
 from neurons import neuron
-from neurons import neuronOutput
-import random
+import numpy as np
 import math
 
-w = [
-    [1,1],
-    [random.random(), random.random()],
-    [random.random(), random.random()],
-    random.random(),
-    random.random()
-]
+w = np.random.random((5,2))
 
 def read_json(file, name):
     f = open(file, "r")
@@ -30,6 +23,7 @@ def estetics_in_json(string):                   # makes the json readable before
     string = string.replace("}", "\n}")
     string = string.replace(', "',',\n"')
     return string
+
 print("Neural Network gets configurated:")
 
 #declaration of neurons
@@ -37,44 +31,69 @@ neuron1 = neuron([1], [w[1][0]])
 neuron2 = neuron([1], [w[1][1]])
 neuron3 = neuron([neuron1.y, neuron2.y], [w[1][0], w[2][0]])
 neuron4 = neuron([neuron1.y, neuron2.y], [w[1][1], w[2][1]])
-neuron5 = neuron([neuron3.y, neuron4.y], [w[3], w[4]])
+neuron5 = neuron([neuron3.y, neuron4.y], [w[3][0], w[4][0]])
+
+print("first neuron1: "+str(neuron1.z) + "  " + str(neuron1.y))
+print("first neuron2: "+str(neuron2.z) + "  " + str(neuron2.y))
+print("first neuron3: "+str(neuron3.z) + "  " + str(neuron3.y))
+print("first neuron4: "+str(neuron4.z) + "  " + str(neuron4.y))
+print("first neuron5: "+str(neuron5.z) + "  " + str(neuron5.y))
 
 print("finished\n")
 
-print("input values gets configurated")
+print("input values gets configurated:")
 x = read_json("vectors_new.json", "frequency")
-for a in x:
-    a = a/math.pow(10,8)
-print("finished\n")
+y = read_json("vectors_new.json", "photosynthetic_activity")
 
+for i in range(len(x)):
+    y[i] = float(y[i]*10e5)
+
+print("neural input: " + str(x))
+print("neural output: "+ str(y))
+
+print("finished\n")
 print("training of neural network:")
+eta = read_json("vectors_new.json", "etha")
+n = 2
 
 # loop for training index
 for running_index in range(read_json("vectors_new.json", "running_time_training")):
-
-#    print("\nweight 1/0: " + str(w[1][0]) + "   |   weight 1/1: " + str(w[1][1]))
-#    print("weight 1/0:" + str(w[2][0]) + "   |   weight 1/1:" + str(w[2][1]))
-#    print("weight 3/0:" + str(w[3]) + "   |   weight 4/0:" + str(w[4]))
-
+    sume = 0.0
     # loop for length of input
     for i in range(len(x)):
         neuron1.update([x[i]], [w[1][0]])
         neuron2.update([1], [w[1][1]])
         neuron3.update([neuron1.y, neuron2.y], [w[1][0], w[2][0]])
         neuron4.update([neuron1.y, neuron2.y], [w[1][1], w[2][1]])
-        neuron5.update([neuron3.y, neuron4.y], [w[3], w[4]])
-        e = read_json("vectors_new.json", "photosynthetic_activity")[i] - neuron5.y
+        neuron5.update([neuron3.y, neuron4.y], [w[3][0], w[4][0]])
+        e = y[i] - neuron5.y
 
-        w[3] += 2 * e * neuron5.z * (1 - neuron5.z) * neuron3.y * read_json("vectors_new.json","etha")
-        w[4] += 2 * e * neuron5.z * (1 - neuron5.z) * neuron4.y * read_json("vectors_new.json","etha")
-        w[1][0] += 2 * e * neuron5.z * (1 - neuron5.z) * w[3] * neuron3.z * (1 - neuron3.z) * neuron1.y * read_json("vectors_new.json","etha")
-        w[1][1] += 2 * e * neuron5.z * (1 - neuron5.z) * w[4] * neuron4.z * (1 - neuron4.z) * neuron1.y * read_json("vectors_new.json","etha")
-        w[2][0] += 2 * e * neuron5.z * (1 - neuron5.z) * w[3] * neuron3.z * (1 - neuron3.z) * neuron2.y * read_json("vectors_new.json","etha")
-        w[2][1] += 2 * e * neuron5.z * (1 - neuron5.z) * w[4] * neuron4.z * (1 - neuron4.z) * neuron2.y * read_json("vectors_new.json","etha")
+        w[3][0] += 2 * e * neuron5.z * (1 - neuron5.z) * neuron3.y * eta * (1+np.abs(np.power(e,n)))
+        w[4][0] += 2 * e * neuron5.z * (1 - neuron5.z) * neuron4.y * eta * (1+np.abs(np.power(e,n)))
+        w[1][0] += 2 * e * neuron5.z * (1 - neuron5.z) * w[3][0] * neuron3.z * (1 - neuron3.z) * neuron1.y * eta * (1+np.abs(np.power(e,n)))
+        w[1][1] += 2 * e * neuron5.z * (1 - neuron5.z) * w[4][0] * neuron4.z * (1 - neuron4.z) * neuron1.y * eta * (1+np.abs(np.power(e,n)))
+        w[2][0] += 2 * e * neuron5.z * (1 - neuron5.z) * w[3][0] * neuron3.z * (1 - neuron3.z) * neuron2.y * eta * (1+np.abs(np.power(e,n)))
+        w[2][1] += 2 * e * neuron5.z * (1 - neuron5.z) * w[4][0] * neuron4.z * (1 - neuron4.z) * neuron2.y * eta * (1+np.abs(np.power(e,n)))
+        sume+= np.abs(e)
 
+        #print("first neuron1: " + str(neuron1.z) + "  " + str(neuron1.y))
+        #print("first neuron2: " + str(neuron2.z) + "  " + str(neuron2.y))
+        #print("first neuron3: " + str(neuron3.z) + "  " + str(neuron3.y))
+        #print("first neuron4: " + str(neuron4.z) + "  " + str(neuron4.y))
+        #print("first neuron5: " + str(neuron5.z) + "  " + str(neuron5.y))
+        print("error: "+str(e))
+    print("---------------------------")
+    print("sum of error: "+ str(sume))
+    print("---------------------------")
 print("finished\n")
-print("safe neural configuration of weights")
-data = str({"weight1/0":w[1][0], "weight1/1": w[1][1], "weight2/0": w[2][0], "weight2/1": w[2][1], "weight3":w[3], "weight4": w[4], "etha": read_json("vectors_new.json", "etha"), "running_index": read_json("vectors_new.json", "running_time_training")})#write_json("neural_config.json", data)
+
+print("\nweight 1/0: " + str(w[1][0]) + "   |   weight 1/1: " + str(w[1][1]))
+print("weight 2/0:" + str(w[2][0]) + "   |   weight 2/1:" + str(w[2][1]))
+print("weight 3/0:" + str(w[3][0]) + "   |   weight 4/0:" + str(w[4][0]))
+
+print("safe neural configuration of weights:")
+data = str({"weight1/0":w[1][0], "weight1/1": w[1][1], "weight2/0": w[2][0], "weight2/1": w[2][1], "weight3":w[3][0], "weight4": w[4][0], "etha": read_json("vectors_new.json", "etha"), "running_index": read_json("vectors_new.json", "running_time_training")})#write_json("neural_config.json", data)
+write_json("neural_config.json", data)
 print("finished\n")
 
 print("performe test:")
@@ -85,10 +104,11 @@ for i in range(len(x)):
     neuron2.update([1], [w[1][1]])
     neuron3.update([neuron1.y, neuron2.y], [w[1][0], w[2][0]])
     neuron4.update([neuron1.y, neuron2.y], [w[1][1], w[2][1]])
-    neuron5.update([neuron3.y, neuron4.y], [w[3], w[4]])
+    neuron5.update([neuron3.y, neuron4.y], [w[3][0], w[4][0]])
     new_y.append(neuron5.y)
 
-print(read_json("vectors_new.json", "photosynthetic_activity"))
+#print(read_json("vectors_new.json", "photosynthetic_activity"))
+print(y)
 print(new_y)
 print("finished\n")
 

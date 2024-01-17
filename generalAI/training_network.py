c5da6fa714fa348b1624ac8bd04419c8dbce6527
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2024-01-17 21:22:06 +0100

    first step of ai learning

--- generalAI/training_network.py
+++ generalAI/training_network.py
@@ -33,12 +33,13 @@ i1 = neuron(); i2 = neuron(); i3 = neuron(); i4 = neuron(); h1 = neuron(); h2 = 
 # declace weights
 iw = np.random.random((4, 4))
 hw = np.random.random((4, 2))
+ow = np.random.random((2, 1))
 
 # declare scale factor
 print("- define scale factors")
-scale_light = 10e2
+scale_light = 10e-5
 scale_CO2 = 1
-scale_temp = 0.1
+scale_temp = 1/100
 print("finished")
 
 # declare input data
@@ -46,13 +47,26 @@ print("- load input data")
 input_light = np.dot(read_input("./../input_data.json", "general_regression", "light_intensity"),scale_light)
 input_co2 = np.dot(read_input("./../input_data.json", "general_regression", "CO2_proportion"),scale_CO2)
 input_temp = np.dot(read_input("./../input_data.json", "general_regression", "ambient_temperature"),scale_temp)
+print("scaled light: " + str(input_light[5]))
+print("scaled CO2-proportion: " + str(input_co2[5]))
+print("scaled temperature: " + str(input_temp[5]))
 print("finished")
 
 # adapt weights
 print("- start training of network")
+eta = read_input("./../input_data.json", "general_regression", "eta")
 for running_index in range(read_input("./../input_data.json", "general_regression", "running_time_training")):
-    for value in range(len(input_temp)):
-        pass
+    for x in range(len(input_temp)):
+
+        # calc actual network output
+        i1.update([input_light[x], input_co2[x], input_temp[x], 1], [iw[0][0], iw[1][0], iw[2][0], iw[3][0]])
+        i2.update([input_light[x], input_co2[x], input_temp[x], 1], [iw[0][1], iw[1][1], iw[2][1], iw[3][1]])
+        i3.update([input_light[x], input_co2[x], input_temp[x], 1], [iw[0][2], iw[1][2], iw[2][2], iw[3][2]])
+        i4.update([input_light[x], input_co2[x], input_temp[x], 1], [iw[0][3], iw[1][3], iw[2][3], iw[3][3]])
+        h1.update([i1.y, i2.y, i3.y, i4.y], [hw[0][0], hw[1][0], hw[2][0], hw[3][0]])
+        h2.update([i1.y, i2.y, i3.y, i4.y], [hw[0][1], hw[1][1], hw[2][1], hw[3][1]])
+        o.update([h1.y, h2.y],[ow[0], ow[1]])
+        print(o.y)
 print("finished")
 
 print("- storage data")
@@ -60,4 +74,3 @@ old_data = read_accuracy("linear_regression")
 old_data.update(read_accuracy("sigmoid_regression"))
 #new_data = "general_regression": {"calc_co2_capture_capacity": calc_data}
 print("finished")
-s

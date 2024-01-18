265205cf1fcb83d018e76bdbdc8936563a714390
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2024-01-18 22:59:26 +0100

    made weight adaption (horrible)

--- generalAI/training_network.py
+++ generalAI/training_network.py
@@ -47,6 +47,7 @@ print("- load input data")
 input_light = np.dot(read_input("./../input_data.json", "general_regression", "light_intensity"),scale_light)
 input_co2 = np.dot(read_input("./../input_data.json", "general_regression", "CO2_proportion"),scale_CO2)
 input_temp = np.dot(read_input("./../input_data.json", "general_regression", "ambient_temperature"),scale_temp)
+y_data = read_input("./../input_data.json", "general_regression", "CO2_capture_capacity")
 print("scaled light: " + str(input_light[5]))
 print("scaled CO2-proportion: " + str(input_co2[5]))
 print("scaled temperature: " + str(input_temp[5]))
@@ -67,6 +68,44 @@ for running_index in range(read_input("./../input_data.json", "general_regressio
         h2.update([i1.y, i2.y, i3.y, i4.y], [hw[0][1], hw[1][1], hw[2][1], hw[3][1]])
         o.update([h1.y, h2.y],[ow[0], ow[1]])
         print(o.y)
+
+        error = y_data[x] - o.y
+
+        # adaption for output weights
+        ow[0] += 2*error*o.y*(1-o.y)*h1.y*eta
+        ow[1] += 2*error*o.y*(1-o.y)*h2.y*eta
+
+        # adaption for hidden weights
+        hw[0][0] += 2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * i1.y * eta
+        hw[0][1] += 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * i1.y * eta
+        hw[1][0] += 2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * i2.y * eta
+        hw[1][1] += 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * i2.y * eta
+        hw[2][0] += 2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * i3.y * eta
+        hw[2][1] += 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * i3.y * eta
+        hw[3][0] += 2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * i4.y * eta
+        hw[3][1] += 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * i4.y * eta
+
+        # adaption for input weights
+        iw[0][0] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[0][0] * i1.y * (1-i1.y) * input_light[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[0][1] * i1.y * (1-i1.y) * input_light[x]) * eta
+        iw[0][1] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[1][0] * i2.y * (1-i2.y) * input_light[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[1][1] * i2.y * (1-i2.y) * input_light[x]) * eta
+        iw[0][2] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[2][0] * i3.y * (1-i2.y) * input_light[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[2][1] * i3.y * (1-i3.y) * input_light[x]) * eta
+        iw[0][3] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[3][0] * i4.y * (1-i4.y) * input_light[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[3][1] * i4.y * (1-i4.y) * input_light[x]) * eta
+
+        iw[1][0] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[0][0] * i1.y * (1-i1.y) * input_co2[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[0][1] * i1.y * (1-i1.y) * input_co2[x]) * eta
+        iw[1][1] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[1][0] * i2.y * (1-i2.y) * input_co2[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[1][1] * i2.y * (1-i2.y) * input_co2[x]) * eta
+        iw[1][2] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[2][0] * i3.y * (1-i3.y) * input_co2[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[2][1] * i3.y * (1-i3.y) * input_co2[x]) * eta
+        iw[1][3] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[3][0] * i4.y * (1-i4.y) * input_co2[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[3][1] * i4.y * (1-i4.y) * input_co2[x]) * eta
+
+        iw[2][0] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[0][0] * i1.y * (1-i1.y) * input_temp[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[0][1] * i1.y * (1-i1.y) * input_temp[x]) * eta
+        iw[2][1] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[1][0] * i2.y * (1-i2.y) * input_temp[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[1][1] * i2.y * (1-i2.y) * input_temp[x]) * eta
+        iw[2][2] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[2][0] * i3.y * (1-i3.y) * input_temp[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[2][1] * i3.y * (1-i3.y) * input_temp[x]) * eta
+        iw[2][3] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[3][0] * i4.y * (1-i4.y) * input_temp[x] + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[3][1] * i4.y * (1-i4.y) * input_temp[x]) * eta
+
+        iw[3][0] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[0][0] * i1.y * (1-i1.y) + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[0][1] * i1.y * (1-i1.y)) * eta
+        iw[3][1] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[1][0] * i2.y * (1-i2.y) + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[1][1] * i2.y * (1-i2.y)) * eta
+        iw[3][2] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[2][0] * i3.y * (1-i3.y) + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[2][1] * i3.y * (1-i3.y)) * eta
+        iw[3][3] += (2*error*o.y*(1-o.y) * ow[0] * h1.y * (1-h1.y) * hw[3][0] * i4.y * (1-i4.y) + 2*error*o.y*(1-o.y) * ow[1] * h2.y * (1-h2.y) * hw[3][1] * i4.y * (1-i4.y)) * eta
+
 print("finished")
 
 print("- storage data")

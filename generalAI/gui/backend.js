6ca3e56d4b930d65dc98b8a53308ffda8613f34c
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2024-01-24 23:38:19 +0100

    continued

--- generalAI/gui/backend.js
+++ generalAI/gui/backend.js
@@ -1,3 +1,5 @@
+import data from "./result.json"
+
 // for the light slidecontainer
 var slider_light = document.getElementById("myRange_light");
 var output_light = document.getElementById("actualval_light");
@@ -16,10 +18,32 @@ output_temp.innerHTML = slider_temp.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider_light.oninput = function() {
   output_light.innerHTML = this.value;
+  request_to_classifier(output_light, output_co2, output_temp)
+  var result = read_json()
+  document.getElementById("result").innerHTML = result
 } 
 slider_co2.oninput = function() {
     output_co2.innerHTML = this.value;
+    request_to_classifier(output_light, output_co2, output_temp)
+    var result = read_json()
+    document.getElementById("result").innerHTML = result
   } 
 slider_temp.oninput = function() {
     output_temp.innerHTML = this.value;
+    request_to_classifier(output_light, output_co2, output_temp)
+    var result = read_json()
+    document.getElementById("result").innerHTML = result
 } 
+
+function request_to_classifier(light, co2, temp){
+    $.ajax({
+        url: './../classifier.py',
+        data: {param: {light, co2, temp}},
+        type: 'POST',
+      }).done(function() {
+       /* Process the data */
+     });
+}
+function read_json(){
+    return data["result"]
+}
